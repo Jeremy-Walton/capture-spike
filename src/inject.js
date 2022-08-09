fe8bda72e7601e4b6c3a21ce71356cdafa4607ff
Josh McLeod <josh@mcleodfamily.org>
2022-08-09 16:17:11 -0400

    Esbundle juice converting html code

--- src/inject.js
+++ src/inject.js
@@ -1,11 +1,21 @@
+const juice = require('juice')
+
 let iframe = document.createElement('iframe');
+
 iframe.src = window.location;
 iframe.style.position = 'absolute';
-iframe.style.left = '10px';
-iframe.style.top = '10px';
+iframe.style.left = '5vw';
+iframe.style.top = '5vh';
 iframe.style.width = '95vh';
 iframe.style.height = '95vh';
 iframe.style.zIndex = '9999';
 iframe.style.border = '1px solid red';
 
 document.body.appendChild(iframe);
+
+console.log('Juicing up: ')
+
+const juicedUpCode = juice(iframe.contentWindow.document.body.innerHTML)
+console.log('Juiced html: ', juicedUpCode)
+navigator.clipboard.writeText(juicedUpCode)
+
